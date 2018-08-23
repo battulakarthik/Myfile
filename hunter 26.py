d4ed85af8981a107ec822bf81127bd0f0ffbe58c
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-08-23 12:08:39 +0530

    Update hunter 26.py

--- hunter 26.py
+++ hunter 26.py
@@ -1,7 +1,8 @@
 n=int(input())
-lst=[int(x) for x in input().split()]
-for i in range(n-1,-1,-1):
-    if i>0:
-        k="->"
-    else:k=''
-    print(lst[i],end=k)
+if n>=1 and n<=10000:
+    lst=[int(x) for x in input().split()]
+    for i in range(n-1,-1,-1):
+        if i>0:
+            k="->"
+        else:k=''
+        print(lst[i],end=k)
